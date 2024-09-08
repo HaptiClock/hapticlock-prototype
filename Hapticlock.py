5f4e9c2f48079a1dc5ecc7972aa3e64e6e795866
lorenzohess <lorenzohess@tutanota.com>
2024-09-07 20:23:19 -0400

    Use EffectLink instead of four-tuple

--- Hapticlock.py
+++ Hapticlock.py
@@ -14,6 +14,14 @@ import time
 # EffectChainType = list[tuple[int, float, float, str]]
 
 
+class EffectLink:
+    def __init__(self, effect_id, effect_duration, sleep_duration, buzzer):
+        self.effect_id = effect_id
+        self.effect_duration: float = effect_duration
+        self.sleep_duration: float = sleep_duration
+        self.buzzer: str = buzzer
+
+
 class TimeProtocolHHMM:
     # """A base class for a Time Protocol for transmitting hours and minutes."""
 
@@ -57,7 +65,8 @@ class TimeProtocolHHLeftMMRight(TimeProtocolHHMM):
             id = self.timeThresholdEffectMap["12hr"]
             effect_duration = 0.5
             sleep_duration = 0.4
-            hoursChain.append((id, effect_duration, sleep_duration, "L"))
+            hoursChain.append(EffectLink(id, effect_duration, sleep_duration, "L"))
+            # hoursChain.append((id, effect_duration, sleep_duration, "L"))
             # Decrement time for the next hours iteration
             HH -= 12
         # Add effects for the remaining hours
@@ -65,7 +74,8 @@ class TimeProtocolHHLeftMMRight(TimeProtocolHHMM):
             id = self.timeThresholdEffectMap["1hr"]
             effect_duration = 0.65
             sleep_duration = 0.2
-            hoursChain.append((id, effect_duration, sleep_duration, "L"))
+            hoursChain.append(EffectLink(id, effect_duration, sleep_duration, "L"))
+            # hoursChain.append((id, effect_duration, sleep_duration, "L"))
         return hoursChain
 
     def _generateMinutesEffectChain(self, MM: int):
@@ -76,7 +86,8 @@ class TimeProtocolHHLeftMMRight(TimeProtocolHHMM):
             id = self.timeThresholdEffectMap["30min"]  # soft bump for 30 minutes
             effect_duration = 0.5
             sleep_duration = 0.4
-            minutesChain.append((id, effect_duration, sleep_duration, "R"))
+            minutesChain.append(EffectLink(id, effect_duration, sleep_duration, "R"))
+            # minutesChain.append((id, effect_duration, sleep_duration, "R"))
             # Decrement time for the next hours iteration
             MM -= 30
         # Add effects for the remaining hours
@@ -84,14 +95,16 @@ class TimeProtocolHHLeftMMRight(TimeProtocolHHMM):
             id = self.timeThresholdEffectMap["5min"]  # sharp click for 5 minutes
             effect_duration = 0.5
             sleep_duration = 0.2
-            minutesChain.append((id, effect_duration, sleep_duration, "R"))
+            minutesChain.append(EffectLink(id, effect_duration, sleep_duration, "R"))
+            # minutesChain.append((id, effect_duration, sleep_duration, "R"))
         return minutesChain
 
     def generateEffectChain(self, HH, MM):
         """Return an EffectChain for HHMM."""
         # TODO Make EffectChain data class
         effectChain = self._generateHoursEffectChain(HH)
-        effectChain.append((-1, 0, 0, self.HHMMseparation))
+        effectChain.append(EffectLink(-1, 0, 0, self.HHMMseparation))
+        # effectChain.append((-1, 0, 0, self.HHMMseparation))
         effectChain = effectChain + self._generateMinutesEffectChain(MM)
         return effectChain
 
@@ -103,7 +116,7 @@ class BuzzerController:
         self.buzzerLeft = buzzerLeft
         self.buzzerRight = buzzerRight
 
-    def playEffectChain(self, effectChain: list[tuple[int, float, float, str]]):
+    def playEffectChain(self, effectChain: list[EffectLink]):
         """Play effects from a chain.
         Buzz a chain of effects with pauses in between.
 
@@ -111,19 +124,20 @@ class BuzzerController:
         """
         # drv.sequence[1] = adafruit_drv2605.Pause(0.5)  # Pause for half a second
         for effectLink in effectChain:
-            id, effect_duration, sleep_duration, buzzer_id = effectLink
+            # id, effect_duration, sleep_duration, buzzer_id = effectLink
             # Sleep between effects if effect id is -1
-            if id == -1:
-                time.sleep(sleep_duration)
+            if effectLink.effect_id == -1:
+                time.sleep(effectLink.sleep_duration)
             else:
-                self.playEffectOnBuzzer(id, effect_duration, buzzer_id)
+                self.playEffectOnBuzzer(
+                    effectLink.effect_id, effectLink.effect_duration, effectLink.buzzer
+                )
 
     def playEffectOnBuzzer(self, id, effect_duration, buzzer_id):
         """Play an effect on the correct buzzer."""
         if buzzer_id == "L":
             self.buzzerLeft.buzzEffectWithDuration(id, effect_duration)
         else:
-            print("Buzz on right")
             self.buzzerRight.buzzEffectWithDuration(id, effect_duration)
 
 
@@ -248,9 +262,9 @@ class Hapticlock:
     def buzzTime(self):
         """Buzz the time to the user."""
         HH, MM = self.getHHMM()
-        # print(f"Buzzing time:  {HH}:{MM}")
+        print(f"Buzzing time:  {HH}:{MM}")
         effectChain = self.time_protocol.generateEffectChain(HH, MM)
-        # print(f"Effect chain: {effectChain}")
+        print(f"Effect chain: {effectChain}")
         self.buzzer_controller.playEffectChain(effectChain)
 
     # def checkForceEvents(self):
