a2aa02a9cf2915052766ed76beee2050b380a724
lorenzohess <lorenzohess@tutanota.com>
2024-09-07 20:21:42 -0400

    Working wifi connection and NTP time

--- Hapticlock.py
+++ Hapticlock.py
@@ -7,6 +7,8 @@ import board
 import busio
 import gc
 import machine
+import network
+import ntptime
 import time
 
 # EffectChainType = list[tuple[int, float, float, str]]
@@ -157,6 +159,8 @@ class Hapticlock:
     def __init__(self):
         # Event loop sleep time between repeats
         self.EVENT_LOOP_SLEEP = 5
+        self.WIFI_CONNECT_SLEEP = 1
+        self.EST_TIMEZONE_OFFSET = -4 * 3600  # UTC-4, in seconds
         # Time protocol
         self.time_protocol = TimeProtocolHHLeftMMRight()
         # Buzzer controller
@@ -177,9 +181,10 @@ class Hapticlock:
         self.HAPTIC_CONTROLLER_CLOCK_GP_RIGHT = board.GP13
 
         # RTC
-        # self.rtc = DS3231()
         self.initializeComponents()
-        # self.buzzer_controller = BuzzerController(self.buzzerLeft, self.buzzerRight)
+        # self.rtc = DS3231()
+        # self.buzzerLeft = []
+        self.buzzer_controller = BuzzerController(self.buzzerLeft, self.buzzerRight)
 
     # def initializeCapacitiveTouch(self):
     #     """Initialize the capacitive touch breakout board."""
@@ -231,7 +236,11 @@ class Hapticlock:
         # timeTuple = (hour, minute)
         # timeTuple = (15, 26)
         # return (18, 26)
-        _, _, _, _, hour, minute, _ = self.rtc.DS3231_ReadTime(mode=1)
+        # _, _, _, _, hour, minute, _ = self.rtc.DS3231_ReadTime(mode=1)
+        # year, month, mday, hour, minute, second, weekday, yearday
+        unix_time_UTC = ntptime.time()
+        unix_time_EST = unix_time_UTC + self.EST_TIMEZONE_OFFSET
+        _, _, _, hour, minute, _, _, _ = ntptime.utime.localtime(unix_time_EST)
         return hour, minute
 
     # def buzzAlert(self):
@@ -260,14 +269,37 @@ class Hapticlock:
     #         print("Both capacitive sensors touched.")
     #         self.buzzTime()
 
+    def connectWifi(self):
+        """Connect to WiFi."""
+        # Check if already connected
+        wlan = network.WLAN(network.STA_IF)
+        if not wlan.isconnected():
+            ssid = "vscode"
+            with open("vscode.password", "r") as passfile:
+                password = passfile.read().strip()
+
+            wlan = network.WLAN(network.STA_IF)
+            wlan.active(True)
+            wlan.connect(ssid, password)
+
+            while not wlan.isconnected():
+                print("Connecting to Wi-Fi: '{ssid}'...")
+                time.sleep(self.WIFI_CONNECT_SLEEP)
+                print(f"Connected to Wi-Fi: '{ssid}'.")
+        else:
+            print(f"Already connected to Wi-Fi.")
+
     def run(self):
         """The Hapticlock event loop."""
         # print("Entering event loop.")
-        # self.playAllHapticControllerEffects()
-        while True:
+        self.connectWifi()
+
+        max_runs = 5
+        runs = 0
+        while runs < max_runs:
             gc.collect()
             self.buzzTime()
-            # while True:
+
             # Check FSR
             # self.checkForceEvents()
 
@@ -276,109 +308,10 @@ class Hapticlock:
 
             # Sleep
             time.sleep(self.EVENT_LOOP_SLEEP)
-
-
-# class DS3231:
-#     """Simple DS3231 class."""
-
-#     w = ["FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"]
-#     # If you want different names for Weekdays, feel free to add. Couple examples below:
-#     # w = ["FR", "SA", "SU", "MO", "TU", "WE", "TH"]
-#     # w = ["Friday", "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"]
-#     # w = ["Freitag", "Samstag", "Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag"]
-#     # w = ["viernes", "sabado", "domingo", "lunes", "martes", "miercoles", "jueves"]
-
-#     # Initialisation of RTC object. Several settings are possible but everything is optional.
-#     # If you meet these standards no parameters are required.
-#     def __init__(
-#         #     capacitiveI2C = busio.I2C(
-#         #         self.CAP_TOUCH_BOARD_CLOCK_GP, self.CAP_TOUCH_BOARD_DATA_GP
-#         #     )
-#         #     mpr121 = adafruit_mpr121.MPR121(capacitiveI2C)
-#         self,
-#         sda_pin=16,
-#         scl_pin=17,
-#         port=0,
-#         speed=100000,
-#         address=0x68,
-#         register=0x00,
-#     ):
-#         self.rtc_address = address  # for using different i2c address
-#         self.rtc_register = (
-#             register  # for using different register on device. DON'T change for DS3231
-#         )
-#         # Old I2C
-#         sda = machine.Pin(sda_pin)  # configure the sda pin
-#         scl = machine.Pin(scl_pin)  # configure the scl pin
-#         self.i2c = machine.I2C(
-#             port, sda=sda, scl=scl, freq=speed
-#         )  # configure the i2c interface with given parameters
-
-#     # Method for setting the Time
-#     # def DS3231_SetTime(self, NowTime=b"\x00\x23\x12\x28\x14\x07\x21"):
-#     #     # NowTime has to be in format like b'\x00\x23\x12\x28\x14\x07\x21'
-#     #     # It is encoded like this           sec min hour week day month year
-#     #     # Then it's written to the DS3231
-#     #     self.i2c.writeto_mem(int(self.rtc_address), int(self.rtc_register), NowTime)
-
-#     # DS3231 gives data in bcd format. This has to be converted to a binary format.
-#     def bcd2bin(self, value):
-#         return (value or 0) - 6 * ((value or 0) >> 4)
-
-#     # Add a 0 in front of numbers smaller than 10
-#     def pre_zero(self, value):
-#         pre_zero = True  # Change to False if you don't want a "0" in front of numbers smaller than 10
-#         if pre_zero:
-#             if value < 10:
-#                 value = f"0{value}"  # From now on the value is a string!
-#         return value
-
-#     # Read the Realtime from the DS3231 with errorhandling. Currently two output modes can be used.
-#     def DS3231_ReadTime(self, mode=0) -> tuple:
-#         try:
-#             # Read RT from DS3231 and write to the buffer variable. It's a list with 7 entries.
-#             # Every entry needs to be converted from bcd to bin.
-#             buffer = self.i2c.readfrom_mem(self.rtc_address, self.rtc_register, 7)
-#             # The year consists of 2 digits. Here 2000 years are added to get format like "2021"
-#             # year = self.bcd2bin(buffer[6]) + 2000
-#             # Just put the month value in the month variable and convert it.
-#             # month = self.bcd2bin(buffer[5])
-#             # Same for the day value
-#             # day = self.bcd2bin(buffer[4])
-#             # Weekday will be converted in the weekdays name or shortform like "Sunday" or "SUN"
-#             # weekday = self.w[self.bcd2bin(buffer[3])]
-#             # Uncomment the line below if you want a number for the weekday and comment the line before.
-#             # weekday = self.bcd2bin(buffer[3])
-#             # Convert bcd to bin and add a "0" if necessary
-#             hour = self.pre_zero(self.bcd2bin(buffer[2]))
-#             # Convert bcd to bin and add a "0" if necessary
-#             minute = self.pre_zero(self.bcd2bin(buffer[1]))
-#             # Convert bcd to bin and add a "0" if necessary
-#             # second = self.pre_zero(self.bcd2bin(buffer[0]))
-#             # return (year, month, day, weekday, hour, minute, second)
-#             return (hour, minute)
-#             # if mode == 0:  # Mode 0 returns a list of second, minute, ...
-#             #     return second, minute, hour, weekday, day, month, year
-#             # if mode == 1:
-#             # Mode 1 returns a formated string with time, weekday and date
-#             # time_string = (
-#             #     f"{hour}:{minute}:{second}      {weekday} {day}.{month}.{year}"
-#             # )
-#             # return (year, month, day, weekday, hour, minute, second)
-#             # return time_string
-#             # If you need different format, feel free to add
-
-#         except Exception as e:
-#             raise e
-#             # return (
-#             #     "Error: is the DS3231 not connected or some other problem (%s)" % e
-#             # )  # exception occurs in any case of error.
+            runs += 1
 
 
 if __name__ == "__main__":
     # pass
     hapticlock = Hapticlock()
-    # rtc = DS3231()
-    # print(rtc.DS3231_ReadTime())
-    # hapticlock.testRTC()
-    # hapticlock.run()
+    hapticlock.run()
