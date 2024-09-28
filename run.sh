74d5db099604054c288b4649c5a638b0656e0639
lorenzohess <lorenzohess@tutanota.com>
2024-09-28 16:31:01 -0400

    Update run.sh to run HaptiClock after copying files

--- run.sh
+++ run.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 
 # Minify Hapticlock.py, compile it to mpy, and copy it to Pico W.
+# Also copy over other relevant files.
+# Then soft-reset Pico to run main.py.
+
 ./minify.sh
 
 mpy-cross Hapticlock_min.py
 
 sudo mpremote cp Hapticlock_min.mpy :lib/Hapticlock_min.mpy
+sudo mpremote cp index.html :index.html
+sudo mpremote cp settings.html :settings.html
+sudo mpremote cp settings.json :settings.json
+
+sudo mpremote soft-reset
+
+# Run HaptiClock
+sudo mpremote exec 'import main'
